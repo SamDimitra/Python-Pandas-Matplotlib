cf4df2e186bbcf13a693e8505f4d724b1fdd7ae8
SamDimitra <82901911+SamDimitra@users.noreply.github.com>
2021-11-18 20:10:11 +0200

    Update Pandas-Matplotlib-2.py

--- exercise2/Pandas-Matplotlib-2.py
+++ exercise2/Pandas-Matplotlib-2.py
@@ -1,30 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[42]:
+
 
 
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-
-# In[43]:
-
-
 age=[24,28,30,35,45,50]
 salary_python=[800,1200,1400,1500,1600,2000]
-
-
-# In[44]:
-
 
 plt.plot(age,salary_python,color='red',linestyle='--',marker='.')
 plt.xlabel('Age')
 plt.ylabel('Monthly Salary')
-
-
-# In[45]:
 
 
 salary_js=[700,900,1100,1200,1400,1800]
@@ -35,9 +24,6 @@ plt.ylabel('Monthly Salary')
 plt.legend()
 
 
-# In[62]:
-
-
 
 x_indexes=np.arange(len(age))
 width=0.7
@@ -46,7 +32,7 @@ plt.bar(x_indexes+width,salary_js,color='blue',width=width)
 plt.xticks(range(7), ['0','25','30','35','40','45','50'], fontsize=8, rotation=90)
 
 
-# In[ ]:
+
 
 
 
